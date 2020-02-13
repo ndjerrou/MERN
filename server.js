e707cceb23cb0cfe1cd7113c45e8b5b0ed1dc80b
ndjerrou <modernejd@gmail.com>
2020-02-13 05:30:15 +0100

    User model defined

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ const express = require("express");
 const connectDB = require("./config/db");
 
 const app = express();
+
 connectDB();
+
+app.get("/", (req, res) => res.send("API Running"));
+
+//Define routes
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
-});
-
-app.get("/", (req, res) => {
-  res.send("API Running");
 });
